82eb271219f6d5d11962315985cb2392937f2d58
Mejia16 <66097783+sebastianmejia9916@users.noreply.github.com>
2023-09-06 21:03:26 -0500

    Add files via upload

---
+++ ImprimirInfo.cs
@@ -0,0 +1,17 @@
+﻿using SuperHeroesApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroesApp
+{
+    internal class ImprimirInfo
+    {
+        public void ImprimirSuperHeroe(ISuperHeroe superHeroe)
+        {
+            Console.WriteLine(superHeroe.GetSuperHeroe());
+        }
+    }
+}
